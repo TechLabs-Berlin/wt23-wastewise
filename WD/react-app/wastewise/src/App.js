d401d9be1489db000434f10ffb5e88ddf82cea30
crftwrks <vierzwei@quantentunnel.de>
2023-02-28 12:14:21 +0000

    set up basic React app

---
+++ WD/react-app/wastewise/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+      <div className="App">
+        <h1>WasteWise</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae, sequi esse cum voluptas neque? Et impedit ut cumque voluptatum corporis similique laboriosam quo. Eveniet itaque officia incidunt aperiam explicabo?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae, sequi esse cum voluptas neque? Et impedit ut cumque voluptatum corporis similique laboriosam quo. Eveniet itaque officia incidunt aperiam explicabo?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae, sequi esse cum voluptas neque? Et impedit ut cumque voluptatum corporis similique laboriosam quo. Eveniet itaque officia incidunt aperiam explicabo?</p>
+      </div>
+    );
+  }
+  
+  export default App;
