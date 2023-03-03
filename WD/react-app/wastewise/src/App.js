b46a6f83069f2a3edc1703f1f3d6021b2d8282dc
crftwrks <vierzwei@quantentunnel.de>
2023-03-03 21:25:45 +0000

    React component TakePicture → changed logic → first start the camera, then show videostream and 'take picture' button, then show photo and 'take another picture' button

--- WD/react-app/wastewise/src/App.js
+++ WD/react-app/wastewise/src/App.js
@@ -9,13 +9,13 @@ const App = () => {
             <SplashScreen animationDelay={1000} />
             <NavBar />
             <main>
-                <h1>WasteWise</h1>
-                <hr />
+                {/* <h1>WasteWise</h1>
+                <hr /> */}
                 <TakePicture />
-                <hr />
+                {/* <hr />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto suscipit ex fugiat, esse obcaecati magni nemo eligendi similique. Recusandae enim odio cumque vero quo quisquam beatae rerum velit, obcaecati minima.</p>
                 <p>Molestiae ipsa velit eligendi dicta ea nemo minus, magni accusamus odit possimus esse iusto cumque laboriosam omnis rerum optio, tempora doloremque incidunt aspernatur deleniti! Optio dolor unde quae iste obcaecati!</p>
-                <p>Quas quam animi esse mollitia ipsum in. Eius aspernatur voluptates, cumque quis neque amet tempora nemo in obcaecati id sequi non laudantium nesciunt fuga aperiam dicta voluptatibus autem blanditiis quos.</p>
+                <p>Quas quam animi esse mollitia ipsum in. Eius aspernatur voluptates, cumque quis neque amet tempora nemo in obcaecati id sequi non laudantium nesciunt fuga aperiam dicta voluptatibus autem blanditiis quos.</p> */}
             </main>
       </div>
     );
