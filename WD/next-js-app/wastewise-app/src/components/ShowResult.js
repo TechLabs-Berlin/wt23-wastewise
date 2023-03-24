a1bdeb6831b24823b711d784e29c8113ebfb33e5
crftwrks <vierzwei@quantentunnel.de>
2023-03-24 10:42:05 +0000

    React → TakePicture now passes the image (temporarily together with a random bin category) to ShowResult, from where the user can go back to TakePicture by pushing the 'reset' button.

--- WD/next-js-app/wastewise-app/src/components/ShowResult.js
+++ WD/next-js-app/wastewise-app/src/components/ShowResult.js
@@ -1,17 +1,15 @@
-import Link from 'next/link';
-
-const ShowResult = ({image, category}) => {
+const ShowResult = ({dataURL, category, handleReset}) => {
 
     const output = [
         {
             class: 'm-fail',
-            h: 'Sorry, we couldn’t find a match. 😞',
-            p: 'Please try again. Make sure you only scan a single item at a time. Also placing the object on a plain background like a tabletop or a wall can help us make a suggestion.'
+            h: <>Sorry, we couldn’t find a match. 😞</>,
+            p: <><b>Please try again.</b> Make sure you only scan a single item at a time. Also placing the object on a plain background like a tabletop or a wall can help us make a suggestion.</>
         },
         {   
             class: 'm-cat1',
-            h: 'Lorem ipsum dolor sit amet consectetur.',
-            p: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi perferendis delectus debitis expedita voluptatem ipsam rerum aperiam ad rem corporis asperiores repudiandae sapiente veritatis illum earum neque accusamus, sequi est?'
+            h: <>You can dispose of this in the <i>organic waste bin.</i></>,
+            p: <>Usually there is one in your backyard. Such a bin is usually brown and labeled <b><i>Biotonne</i></b> or <b><i>Biomüll</i></b>.</>
         },
         {
             class: 'm-cat2',
@@ -56,6 +54,7 @@ const ShowResult = ({image, category}) => {
     ];
 
     const getCategoryDetails = (cat) => {
+        if (cat === 'x') { return output[0] };
         return output[cat];
     };
 
@@ -64,18 +63,18 @@ const ShowResult = ({image, category}) => {
     return (
         <main className="showresult">
             <div className={`showresult__flexbox ${result.class}`}>
-                <img src={image} alt="Your captured photo" className="showresult__photo" />
+                <img src={dataURL} alt="Your captured photo" className="showresult__photo" />
                 <div className="showresult__text-box">
                     <h3>{result.h}</h3>
                     <p>{result.p}</p>
                 </div>
             </div>
-            <Link href="/scan" className="showresult__button">
+            <button className="showresult__button" onClick={handleReset}>
                 <svg className="showresult__button__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="none" d="M0 0h24v24H0V0z"/>
                     <path d="M17.65 6.35a7.95 7.95 0 0 0-6.48-2.31c-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20a7.98 7.98 0 0 0 7.21-4.56c.32-.67-.16-1.44-.9-1.44-.37 0-.72.2-.88.53a5.994 5.994 0 0 1-6.8 3.31c-2.22-.49-4.01-2.3-4.48-4.52A6.002 6.002 0 0 1 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z"/>
                 </svg>              
-            </Link>
+            </button>
         </main>
     );
 }
--- WD/next-js-app/wastewise-app/src/components/TakePicture.js
+++ WD/next-js-app/wastewise-app/src/components/TakePicture.js
@@ -1,10 +1,14 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
+import TakePictureAnimation from './TakePictureAnimation';
+import ShowResult from './ShowResult';
+
 
 const TakePicture = () => {
 
     const camRef = useRef(null);
     const [dataURL, setDataURL] = useState(null);
+    const [category, setCategory] = useState(null);
 
     const videoConstraints = {
         width: 2000,
@@ -19,11 +23,34 @@ const TakePicture = () => {
         setDataURL(img);
     };
 
-    return (
-        <main className="takepicture">
+    const handleReset = () => {
+        setCategory(null);
+        setDataURL(null);
+    };
 
+    useEffect(() => {
+        // don't run initially, run only after user has taken a picture
+        if (!dataURL) return;
+        
+        // calling AI MAGIC → hook not working yet
+        // const result = useIdentifyItem(dataURL);
+
+            /* Temporary: Faking/pretending AI MAGIC with setTimeout and a random category value */
+            const randomTime = Math.floor(Math.random() * 4000 + 1000);
+            setTimeout(() => {
+                if (Math.random() < 0.3) setCategory('x');
+                else {
+                    const random2 = Math.floor(Math.random() * 9 + 1);
+                    setCategory(random2);
+                };
+            }, randomTime);
+
+    }, [dataURL]);
+
+    return (
+        <>
             {!dataURL && (
-                <>
+                <main className="takepicture">
                     <div className="takepicture__media-box">
                         <div className="takepicture__frame">
                             <div className="takepicture__canvas">
@@ -56,49 +83,17 @@ const TakePicture = () => {
                             </g>
                         </svg>
                     </div>
-                </>
+                </main>
             )}
 
-            {dataURL && (
-                <>
-                    <div className="takepicture__media-box">
-                        <div className="takepicture__frame">
-                            <div className="takepicture__canvas">
-
-                                <div className="takepicture__processing-box">
-                                    <img
-                                        src={dataURL}
-                                        alt="the photo you just took in a blurred and animated version"
-                                        className="takepicture__photo u-blur"
-                                    />
-                                    <div className="takepicture__processing-animation">
-                                        <img
-                                            src={dataURL}
-                                            alt="the photo you just took"
-                                            className="takepicture__photo"
-                                        />
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="takepicture__button-box">
-                        <svg className="takepicture__button t-inactive" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 120 120">
-                            <circle className="takepicture__button__background" cx="60" cy="60" r="40"/>
-                            <path className="takepicture__button__background" d="M95.8 108.149a60.004 60.004 0 0 0 20.776-68.129A59.999 59.999 0 0 0 2.021 44.56a60 60 0 0 0 26.1 66.271l5.232-8.343a50.149 50.149 0 0 1-21.816-55.394 50.151 50.151 0 1 1 78.388 53.153l5.876 7.903Z"/>
-                            <mask id="a" width="40" height="36" x="40" y="42" maskUnits="userSpaceOnUse" style={{maskType: 'alpha'}}>
-                                <path fill="#000" fill-rule="evenodd" d="M76 46h-6.34l-2.48-2.7c-.74-.82-1.82-1.3-2.94-1.3h-8.48c-1.12 0-2.2.48-2.96 1.3L50.34 46H44c-2.2 0-4 1.8-4 4v24c0 2.2 1.8 4 4 4h32c2.2 0 4-1.8 4-4V50c0-2.2-1.8-4-4-4ZM60 56a6 6 0 1 0 0 12 6 6 0 0 0 0-12Zm-10 6c0 5.52 4.48 10 10 10s10-4.48 10-10-4.48-10-10-10-10 4.48-10 10Z" clip-rule="evenodd"/>
-                            </mask>
-                            <g mask="url(#a)">
-                                <path className="takepicture__button__icon" d="M36 36h48v48H36z"/>
-                            </g>
-                        </svg>
-                    </div>
-                </>
+            {dataURL && !category && (
+                <TakePictureAnimation dataURL={dataURL} />
             )}
 
-        </main>
+            {dataURL && category && (  
+                <ShowResult dataURL={dataURL} category={category} handleReset={handleReset} />
+            )}
+        </>
     );
 };
 
